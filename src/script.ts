a1206614dda0f99db9e378b6df28d526cad40549
Ryan <ryanmahara@gmail.com>
2022-01-01 20:50:37 -0600

    initial commit

---
+++ src/script.ts
@@ -0,0 +1,20 @@
+let x: number = 5;
+
+console.log(x + 3);
+
+interface User {
+  name: string;
+  id: number;
+}
+
+const user: User = {
+  name: 'Hayes',
+  id: 0,
+};
+
+console.log(user);
+
+const user2: User = {
+  name: 'guy',
+  id: 5,
+};
